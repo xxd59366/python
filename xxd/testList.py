38ae52a70684cd7e5bcd6d7cd7ec1581717236fb
云影之海 <38814215+xxd59366@users.noreply.github.com>
2019-05-08 19:24:33 +0800

    index值变化原因

--- xxd/testList.py
+++ xxd/testList.py
@@ -1,6 +1,9 @@
 # 使用位置参数
 str01='{} is {}'.format('bob', 15)
-str02='{1} is {0}'.format(15, 'bob')
+# str02='{1} is {0}'.format(15, 'bob')
+# 当str01与02值相同时，索引到0就停了，所以原本不会出现index is 1的输出
+# 加上一个感叹号，index输出值出现变化
+str02='{1} is {0}!'.format(15, 'bob')
 
 # 使用关键字参数
 str03='name is {name},age is {age}'.format(name='bob',age=23)
@@ -16,7 +19,7 @@ str07='{:<10} is {:0>8}'.format('bob', 15)
 str08='姓名：{0[0]},年龄：{0[1]}'.format(['bob',23])
 
 s=[str01,str02,str03,str04,str05,str06,str07,str08]
-for i in range(len(s)):
+for i in range(1,len(s)+1):
     print(i,end=' ')
-    print(s[i])
-    print('index is', s.index(s[i]))
+    print(s[i-1])
+    print('index is', s.index(s[i-1]))
